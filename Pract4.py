63ca6e84f7372af5a32eab4a1fa28e69fcd45656
Niraj Badgujar <140939940+Nirajbadgujar@users.noreply.github.com>
2025-05-05 00:56:31 +0530

    Add files via upload

---
+++ Pract4.py
@@ -0,0 +1,8 @@
+def berkeley_algorithm(time_offsets):
+    avg_offset=sum(time_offsets)/ len(time_offsets)
+    adjusted_clocks=[time - avg_offset for time in time_offsets]
+    return adjusted_clocks
+
+time_offsets=[1.2,0.5,-0.3,2.1,-1.0]
+adjusted_clocks= berkeley_algorithm(time_offsets)
+print(adjusted_clocks)
